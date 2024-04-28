452646425e520c4f94a3f6dda27849df39c890aa
srd13120550870 <linchuanyi@bestpay.com.cn>
2024-04-28 18:34:59 +0800

    other

--- lcy-flink-sql-demo/src/main/java/com/lcy/flinksql/demo/TestCollectionForTestConvertTime.java
+++ lcy-flink-sql-demo/src/main/java/com/lcy/flinksql/demo/TestCollectionForTestConvertTime.java
@@ -3,16 +3,73 @@ package com.lcy.flinksql.demo;
 import com.lcy.flinksql.utils.FlinkLocalRunHandler;
 import com.lcy.flinksql.utils.GeneratorDataTool;
 import org.apache.flink.calcite.shaded.com.google.common.collect.Lists;
+import org.apache.flink.configuration.Configuration;
+import org.junit.After;
+import org.junit.Test;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 /**
  * @author: lcy
  * @date: 2023/7/27
  **/
 public class TestCollectionForTestConvertTime {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+    @Test
+    public void t03(){
+        Configuration conf = new Configuration();
+
+        FlinkLocalRunHandler flinkLocalRunHandler = new FlinkLocalRunHandler(conf, true) {
+
+            @Override
+            public List<GeneratorDataInfo> generateData() {
+
+                return Lists.newArrayList();
+            }
+
+            @Override
+            public List<String> generateRunSql() {
+
+
+                String sqlSource = "CREATE TABLE source_datagen ("
+                        + "  id INT,"
+                        + "  age INT,"
+                        + "  ts as PROCTIME()"
+                        + ") WITH ("
+                        + "  'connector'='datagen',"
+                        + "  'rows-per-second'='1',"
+                        + "  'fields.age.max'='100',"
+                        + "  'fields.age.min'='1',"
+                        + "  'fields.id.kind'='sequence',"
+                        + "  'fields.id.start'='1',"
+                        + "  'fields.id.end'='100'"
+                        + ")";
+
+                String sqlSink = "CREATE TABLE upsertSink ("
+                        + "  id INT,"
+                        + "  age INT,"
+                        + "  timeDiff BIGINT"
+                        + ") WITH ("
+                        + "  'connector'='print'"
+                        + ")";
+
+
+                String sqlInsert = "insert into upsertSink select id, age,TIMESTAMPDIFF(SECOND, ts, cast('2024-04-17 00:00:00' as TIMESTAMP)) from source_datagen" ;
+
+                return Lists.newArrayList(sqlSource, sqlSink, sqlInsert);
+            }
+        };
+
+        flinkLocalRunHandler.executeSql();
+    }
+
+    @After
+    public void finish() throws InterruptedException {
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void t02(){
         FlinkLocalRunHandler flinkLocalRunHandler = new FlinkLocalRunHandler() {
 
             @Override
